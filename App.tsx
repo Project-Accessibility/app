5d85d33e708e00b3502b47fb8bd6b094c437be3c
IntissarJ <Intissar_jamal@outlook.com>
2022-03-31 10:12:57 +0200

    SOF-119 Onderdeel details

--- App.tsx
+++ App.tsx
@@ -10,10 +10,11 @@
 
 import React, { useEffect, useState } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
+  //SafeAreaView,
+  //ScrollView,
+  //StatusBar,
+  //StyleSheet,
   Text,
   // useColorScheme,
   View,
@@ -21,11 +22,12 @@ import {
 
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
+  //DebugInstructions,
+  //Header,
+  //LearnMoreLinks,
+  //ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import SectionScreen from './src/screens/SectionScreen';
 import SplashScreen from './src/screens/SplashScreen';
 
 const Section: React.FC<{
@@ -61,12 +63,12 @@ const Section: React.FC<{
 
 const App = () => {
   // const isDarkMode = useColorScheme() === 'dark';
-  const isDarkMode = false;
+  //const isDarkMode = false;
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  //const backgroundStyle = {
+  //  backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  //};
 
   useEffect(() => {
     // Quick way of showing splashscreen for 3 seconds.
@@ -76,32 +78,7 @@ const App = () => {
   if (isLoading) {
     return <SplashScreen />;
   }
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}
-        >
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this screen and then come
-            back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">Read the docs to discover what to do next:</Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+  return <SectionScreen />;
 };
 
 const styles = StyleSheet.create({
