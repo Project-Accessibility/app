43f13b1bc234177e6c43d6cfe21fb0ab02523da4
IntissarJ <Intissar_jamal@outlook.com>
2022-03-31 10:23:49 +0200

    deepscan: removed unused variables

--- App.tsx
+++ App.tsx
@@ -9,57 +9,8 @@
  */
 
 import React, { useEffect, useState } from 'react';
-import {
-  StyleSheet,
-  //SafeAreaView,
-  //ScrollView,
-  //StatusBar,
-  //StyleSheet,
-  Text,
-  // useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  //DebugInstructions,
-  //Header,
-  //LearnMoreLinks,
-  //ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import SectionScreen from './src/screens/SectionScreen';
 import SplashScreen from './src/screens/SplashScreen';
-
-const Section: React.FC<{
-  title: string;
-}> = ({ children, title }) => {
-  // const isDarkMode = useColorScheme() === 'dark';
-  const isDarkMode = false;
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}
-      >
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}
-      >
-        {children}
-      </Text>
-    </View>
-  );
-};
 
 const App = () => {
   // const isDarkMode = useColorScheme() === 'dark';
@@ -80,24 +31,5 @@ const App = () => {
   }
   return <SectionScreen />;
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
