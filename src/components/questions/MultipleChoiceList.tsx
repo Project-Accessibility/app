cef10887bf33ea2407185f2cc3edc586267c15d2
Jaap <jaap@mrproper.dev>
2022-04-07 11:05:58 +0200

    Add multiplechoice

---
+++ src/components/questions/MultipleChoiceList.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import RadioButtonRN from 'radio-buttons-react-native';
+import { QuestionOption } from '../../models/QuestionOption';
+import FONTS from '../../assets/fonts';
+import COLORS from '../../assets/colors';
+
+interface RadioButtonData {
+  label: string;
+}
+
+const MasterContainer = (props: { questionOption: QuestionOption }) => {
+  const [selectedOption, setSelectedOption] = useState<string>();
+  const multipleChoiceQuestions = props.questionOption.extraData as unknown as string[];
+
+  return (
+    <RadioButtonRN
+      data={stringArrayToRadioButtonData(multipleChoiceQuestions)}
+      selectedBtn={(selected: RadioButtonData) => {
+        console.log(selected.label);
+        setSelectedOption(selected.label);
+      }}
+      animationTypes={['zoomIn', 'rotate']}
+      textStyle={styles.text}
+      style={styles.radioButton}
+      activeColor={COLORS.green}
+      icon={<Icon name="check-circle" size={25} color={COLORS.green} />}
+    />
+  );
+};
+
+function stringArrayToRadioButtonData(stringArray: string[]): RadioButtonData[] {
+  return stringArray.map((string) => {
+    return {
+      label: string,
+    };
+  });
+}
+
+const styles = StyleSheet.create({
+  radioButton: {
+    width: '100%',
+  },
+  text: {
+    fontFamily: FONTS.semiBold,
+    fontSize: 25,
+    color: COLORS.black,
+  },
+});
+
+export default MasterContainer;
--- src/data/mockData/MockDataRetriever.ts
+++ src/data/mockData/MockDataRetriever.ts
@@ -1,8 +1,18 @@
+import { QuestionOptionType } from '../../enums/QuestionOptionType';
 import { Questionnaire } from '../../models/Questionnaire';
+import { QuestionOption } from '../../models/QuestionOption';
 import QuestionnaireMock from './questionnaire/mockObject.json';
 
 function getQuestionnaireMock(): Questionnaire {
   return <Questionnaire>(<unknown>QuestionnaireMock);
 }
 
-export { getQuestionnaireMock };
+function getQuestionOptionMock(): QuestionOption {
+  return {
+    id: 1,
+    type: QuestionOptionType.MULTIPLE_CHOICE,
+    extraData: ['Te fel licht', 'Precies goed', 'Te weinig licht'] as any,
+  };
+}
+
+export { getQuestionnaireMock, getQuestionOptionMock };
