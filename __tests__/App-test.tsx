baa644d5ebce5e57a9e54df07d12114d1da76e96
milovdpas <mpmpas1@avans.nl>
2022-04-22 01:29:07 +0200

    fix test

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -6,7 +6,9 @@ import 'react-native';
 import React from 'react';
 import App from '../App';
 
-// Note: test renderer must be required after react-native.
+/**
+ * Note: test renderer must be required after react-native.
+ */
 import renderer from 'react-test-renderer';
 
 jest.useFakeTimers();
