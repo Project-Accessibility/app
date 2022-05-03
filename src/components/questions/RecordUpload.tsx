40780cecd69cee005d57b54738eec72404b0f536
IntissarJ <Intissar_jamal@outlook.com>
2022-05-03 13:08:30 +0200

    recording with all functions works for android, still need to test for ios

--- src/components/questions/RecordUpload.tsx
+++ src/components/questions/RecordUpload.tsx
@@ -1,108 +1,129 @@
-import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import AudioRecorderPlayer, {
+  AVEncoderAudioQualityIOSType,
+  AVEncodingOption,
+  AudioEncoderAndroidType,
+  AudioSet,
+  AudioSourceAndroidType,
+  RecordBackType,
+} from 'react-native-audio-recorder-player';
+import { PermissionsAndroid, Platform, StyleSheet, View } from 'react-native';
+import React, { Component } from 'react';
+
 import Icon from 'react-native-vector-icons/FontAwesome';
 import COLORS from '../../assets/colors';
-import permissionCheck from '../utility/PermissionCheck';
-import { PERMISSIONS } from 'react-native-permissions';
-import AudioRecorderPlayer from 'react-native-audio-recorder-player';
 
-const RecordUpload = () => {
-  const [isPlaying, setIsPlaying] = useState(true);
-  const [isRecording, setIsRecording] = useState(false);
-  const [recordSecs, setRecordSecs] = useState(0);
-  const [recordTime, setRecordTime] = useState('00:00:00');
-  const [currentPositionSec, setCurrentPositionSec] = useState(0);
-  const [currentDurationSec, setCurrentDurationSec] = useState(0);
-  const [playTime, setPlayTime] = useState('00:00:00');
-  const [duration, setDuration] = useState('00:00:00');
-  const [voice, setVoice] = useState('');
+interface State {
+  isPlaying: boolean;
+  isRecording: boolean;
+  recordSecs: number;
+  recordTime: string;
+  currentPositionSec: number;
+  currentDurationSec: number;
+  playTime: string;
+  duration: string;
+}
 
-  const RequestMicPermission = async () => {
-    //check ios record
-    permissionCheck.checkPermission(PERMISSIONS.IOS.MICROPHONE);
+class RecordUpload extends Component<any, State> {
+  private audioRecorderPlayer: AudioRecorderPlayer;
 
-    //check android record
-    permissionCheck.checkPermission(PERMISSIONS.ANDROID.RECORD_AUDIO);
-    permissionCheck.checkPermission(PERMISSIONS.ANDROID.WRITE_EXTERNAL_STORAGE);
-    permissionCheck.checkPermission(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      isRecording: false,
+      isPlaying: false,
+      recordSecs: 0,
+      recordTime: '00:00:00',
+      currentPositionSec: 0,
+      currentDurationSec: 0,
+      playTime: '00:00:00',
+      duration: '00:00:00',
+    };
 
-    onStartRecord();
-  };
+    this.audioRecorderPlayer = new AudioRecorderPlayer();
+    this.audioRecorderPlayer.setSubscriptionDuration(0.1); // optional. Default is 0.5
+  }
 
-  const audioRecorderPlayer = new AudioRecorderPlayer();
+  public render() {
+    return (
+      <>
+        <View style={styles.container}>
+          <View style={styles.rowContainer}>
+            {this.state.isRecording ? (
+              <Icon name="stop" size={48} style={styles.icon} onPress={this.onStopRecord} />
+            ) : (
+              <Icon name="microphone" size={48} style={styles.icon} onPress={this.onStartRecord} />
+            )}
 
-  const onStartRecord = async () => {
-    const result = await audioRecorderPlayer.startRecorder();
-    audioRecorderPlayer.addRecordBackListener((e: { currentPosition: number }) => {
-      setRecordSecs(e.currentPosition);
-      setRecordTime(audioRecorderPlayer.mmssss(Math.floor(e.currentPosition)));
-      return;
-    });
-    setIsRecording(true);
-    console.log(result);
-    setVoice(result);
-  };
-
-  const onStopRecord = async () => {
-    const result = await audioRecorderPlayer.stopRecorder();
-    audioRecorderPlayer.removeRecordBackListener();
-    setRecordSecs(0);
-    setIsRecording(false);
-    console.log(result);
-  };
-
-  const onStartPlay = async () => {
-    console.log('onStartPlay');
-    await audioRecorderPlayer.startPlayer();
-    setIsPlaying(false);
-    audioRecorderPlayer.addPlayBackListener((e: { currentPosition: number; duration: number }) => {
-      setCurrentPositionSec(e.currentPosition);
-      setCurrentDurationSec(e.duration);
-      setPlayTime(audioRecorderPlayer.mmssss(Math.floor(e.currentPosition)));
-      setDuration(audioRecorderPlayer.mmssss(Math.floor(e.duration)));
-      console.log(currentDurationSec, currentPositionSec);
-      return;
-    });
-  };
-
-  const onPausePlay = async () => {
-    console.log('onPausePlay');
-    await audioRecorderPlayer.pausePlayer();
-    setIsPlaying(true);
-  };
-
-  const onStopPlay = async () => {
-    console.log('onStopPlay');
-    audioRecorderPlayer.stopPlayer();
-    audioRecorderPlayer.removePlayBackListener();
-  };
-
-  return (
-    <>
-      <View style={styles.container}>
-        <View style={styles.rowContainer}>
-          {isRecording ? (
-            <Icon name="stop" size={48} style={styles.icon} onPress={() => onStopRecord()} />
-          ) : (
-            <Icon
-              name="microphone"
-              size={48}
-              style={styles.icon}
-              onPress={() => RequestMicPermission()}
-            />
-          )}
-          {isPlaying ? (
-            <Icon name="play" size={48} style={styles.icon} onPress={() => onStartPlay()} />
-          ) : (
-            <Icon name="pause" size={48} style={styles.icon} onPress={() => onPausePlay()} />
-          )}
-          <Icon name="undo" size={48} style={styles.icon} onPress={() => onStopPlay()} />
-          <Icon name="backward" size={48} style={styles.icon} onPress={() => onStopPlay()} />
+            {this.state.isPlaying ? (
+              <Icon name="pause" size={48} style={styles.icon} onPress={this.onPausePlay} />
+            ) : (
+              <Icon name="play" size={48} style={styles.icon} onPress={this.onStartPlay} />
+            )}
+            <Icon name="backward" size={48} style={styles.icon} onPress={this.onStopPlay} />
+          </View>
         </View>
-      </View>
-    </>
-  );
-};
+      </>
+    );
+  }
+
+  private onStartRecord = async () => {
+    this.onStopPlay();
+    if (Platform.OS === 'android') {
+      try {
+        await PermissionsAndroid.requestMultiple([
+          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+          PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+          PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+        ]);
+      } catch (err) {
+        console.warn(err);
+        return;
+      }
+    }
+
+    const audioSet: AudioSet = {
+      AudioEncoderAndroid: AudioEncoderAndroidType.AAC,
+      AudioSourceAndroid: AudioSourceAndroidType.MIC,
+      AVEncoderAudioQualityKeyIOS: AVEncoderAudioQualityIOSType.high,
+      AVNumberOfChannelsKeyIOS: 2,
+      AVFormatIDKeyIOS: AVEncodingOption.aac,
+    };
+
+    const uri = await this.audioRecorderPlayer.startRecorder(undefined, audioSet);
+
+    this.audioRecorderPlayer.addRecordBackListener((e: RecordBackType) => {
+      console.log('record-back', e);
+      this.setState({
+        isRecording: true,
+      });
+    });
+    console.log(`uri: ${uri}`);
+  };
+
+  private onStopRecord = async () => {
+    await this.audioRecorderPlayer.stopRecorder();
+    this.audioRecorderPlayer.removeRecordBackListener();
+    this.setState({
+      isRecording: false,
+    });
+  };
+
+  private onStartPlay = async () => {
+    await this.audioRecorderPlayer.startPlayer();
+    this.setState({ isPlaying: true });
+  };
+
+  private onPausePlay = async () => {
+    this.setState({ isPlaying: false });
+    await this.audioRecorderPlayer.pausePlayer();
+  };
+
+  private onStopPlay = async () => {
+    this.setState({ isPlaying: false });
+    this.audioRecorderPlayer.stopPlayer();
+    this.audioRecorderPlayer.removePlayBackListener();
+  };
+}
 
 const styles = StyleSheet.create({
   container: {
