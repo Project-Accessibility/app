8b3df21f8c33285d376c91b932f286b256cb66f9
milovdpas <mpmpas1@avans.nl>
2022-05-26 01:14:47 +0200

    value can be null

--- src/helpers/formDataHelper.ts
+++ src/helpers/formDataHelper.ts
@@ -3,10 +3,12 @@ import { Question } from '../models/Question';
 
 export function generateFormDataByQuestion(question: Question): FormData {
   const formData = new FormData();
-  formData.append('question', question);
-  question.questionOptions?.forEach((questionOption) => {
-    if (questionOption.answer) {
-      formData.append(getFormDataKey(questionOption.type), questionOption.answer.values);
+  question.options?.forEach((option) => {
+    if (option.answer) {
+      formData.append(
+        getFormDataKey(option.type),
+        getFormDataValue(option.type, option.answer.values)
+      );
     }
   });
   return formData;
@@ -18,5 +20,17 @@ function getFormDataKey(type: QuestionOptionType): string {
       return `${type.toUpperCase()}[]`;
     default:
       return type.toUpperCase();
+  }
+}
+
+function getFormDataValue(
+  type: QuestionOptionType,
+  values: any[]
+): string[] | number[] | string | number {
+  switch (type) {
+    case QuestionOptionType.MULTIPLE_CHOICE:
+      return values;
+    default:
+      return values[0];
   }
 }
