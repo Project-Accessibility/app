6ff5650d35e23ab318a9d7b6011f262833d7c765
Quinten <q.m.b@hotmail.com>
2022-06-04 19:43:47 +0200

    Helpscreen refactor

--- src/screens/HelpScreen.tsx
+++ src/screens/HelpScreen.tsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Image, Linking, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import MasterContainer from '../components/generic/MasterContainer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import FONTS from '../assets/fonts';
 import COLORS from '../assets/colors';
 import ACC_STRS from '../assets/accessibilityStrings';
+import { NavigationContainer, useFocusEffect } from '@react-navigation/native';
+import ParticipantCode from '../data/localStorage/ParticipantCode';
+
+const HelpScreen = () => {
+  const [hasCode, setHasCode] = useState(false);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      checkParticipantCodeAvailable();
+    }, [])
+  );
+
+  const checkParticipantCodeAvailable = async () => {
+    const code = await ParticipantCode.loadCurrentParticipantCodeFromLocalStorage();
+    if (!code) setHasCode(false);
+    else setHasCode(true);
+  };
+
+  // if code entered, show tab screen.
+  if (hasCode) {
+    return (
+      <View style={styles.main}>
+        <NavigationContainer independent={true}>
+          <Tab.Navigator screenOptions={({route, navigation}) => ({
+            headerShown: false,
+            headerStatusBarHeight: 0,
+          })} >
+            <Tab.Screen name={'BedrijfsInformatie'} component={OptionalCompanyHelp} />
+            <Tab.Screen name={'Accessibility'} component={AccessibilityScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </View>
+    );
+  } else {
+    return AccessibilityScreen();
+  }
+
+};
+
 
 const createText = () => {
   return data.map((element, index) => {
@@ -28,9 +67,10 @@ const phoneCall = () => {
   Linking.openURL(phoneNumber);
 };
 
-const HelpScreen = () => {
+
+function AccessibilityScreen() {
   return (
-    <MasterContainer>
+    <View style={styles.tabScreen}>
       <Image
         style={styles.logo}
         source={require('../assets/images/logos/icon_accessibility_logo_RGB.jpg')}
@@ -75,15 +115,66 @@ const HelpScreen = () => {
           {ACC_STRS.contactExtraInfo}
         </Text>
       </View>
-    </MasterContainer>
+    </View>
   );
-};
+}
+
+function OptionalCompanyHelp() {
+  return (
+    <View style={styles.tabScreen}>
+      <View style={styles.contactInfo}>
+        <View>
+          <Text style={styles.h1}>{ACC_STRS.contactTitle}</Text>
+          {/*Creating text elements based on data*/}
+          <View accessible={true}>
+            {createText()}
+          </View>
+        </View>
+
+        <TouchableOpacity
+          style={styles.emailBtn}
+          onPress={() => {
+            Linking.openURL(`mailto: ${ACC_STRS.contactEmail}`);
+          }}
+        >
+          <Text
+            accessible={true}
+            accessibilityHint={ACC_STRS.contactSendEmailHint}
+            style={styles.emailBtnText}
+          >
+            {ACC_STRS.contactSendEmail}
+          </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.emailBtn} onPress={phoneCall}>
+          <Text
+            accessible={true}
+            accessibilityHint={ACC_STRS.contactCallPhoneHint}
+            style={styles.emailBtnText}
+          >
+            {ACC_STRS.contactCallPhone}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
 
 const styles = StyleSheet.create({
+  main:{
+    flex: 1,
+  },
+  tabScreen: {
+    flex: 1,
+  },
   logo: {
     width: '100%',
     height: 100,
     resizeMode: 'contain',
+    marginTop: 0,
   },
   h1: {
     fontFamily: FONTS.semiBold,
@@ -105,7 +196,7 @@ const styles = StyleSheet.create({
   },
   contactInfo: {
     position: 'relative',
-    left: -30,
+    left: -20,
     backgroundColor: COLORS.gray,
     paddingLeft: 10,
     padding: 20,
@@ -143,10 +234,6 @@ const styles = StyleSheet.create({
 });
 
 const data = [
-  // {
-  //   text: ACC_STRS.contactSubtitle,
-  //   style: styles.contactTitle,
-  // },
   {
     text: ACC_STRS.contactStreet,
     style: styles.contactText,
@@ -155,22 +242,6 @@ const data = [
     text: `${ACC_STRS.contactPostalCode} ${ACC_STRS.contactCity}`,
     style: styles.contactText,
   },
-  // {
-  //   text: ACC_STRS.phone,
-  //   style: styles.contactTitle,
-  // },
-  // {
-  //   text: ACC_STRS.contactPhone,
-  //   style: styles.contactText,
-  // },
-  // {
-  //   text: ACC_STRS.email,
-  //   style: styles.contactTitle,
-  // },
-  // {
-  //   text: ACC_STRS.contactEmail,
-  //   style: styles.contactText,
-  // },
 ];
 
 export default HelpScreen;
