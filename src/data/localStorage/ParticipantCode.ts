4eb34e85bf330ba239876b0eb7baf21f34975bd0
Quinten <q.m.b@hotmail.com>
2022-06-07 14:02:51 +0200

    Removed unused import from particpantcode

--- src/data/localStorage/ParticipantCode.ts
+++ src/data/localStorage/ParticipantCode.ts
@@ -1,5 +1,4 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Questionnaire } from '../../models/Questionnaire';
 
 export interface QuestionnaireDisplay {
   code: string;
