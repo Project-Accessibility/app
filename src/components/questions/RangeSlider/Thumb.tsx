f410aca302c5637e02e8c97c8fa4e289c8f8792b
milovdpas <mpmpas1@avans.nl>
2022-06-14 22:12:35 +0200

    make thumb more clear

--- src/components/questions/RangeSlider/Thumb.tsx
+++ src/components/questions/RangeSlider/Thumb.tsx
@@ -1,14 +1,28 @@
 import React, { memo } from 'react';
 import { StyleSheet, View } from 'react-native';
 import COLORS from '../../../assets/colors';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const THUMB_RADIUS = 12;
+const ICON_SIZE = 25;
 
-const Thumb = () => <View style={styles.root} />;
+const Thumb = () => {
+  return (
+    <View style={styles.root}>
+      <Icon name="angle-left" size={ICON_SIZE} color={COLORS.black} style={styles.icon} />
+      <Icon name="angle-right" size={ICON_SIZE} color={COLORS.black} style={styles.icon} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   root: {
-    width: THUMB_RADIUS * 2,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 8,
+    width: THUMB_RADIUS * 4,
     height: THUMB_RADIUS * 2,
     borderRadius: THUMB_RADIUS,
     borderWidth: 0,
@@ -18,6 +32,9 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: -1 },
     shadowOpacity: 0.16,
     shadowRadius: 6,
+  },
+  icon: {
+    marginBottom: -3,
   },
 });
 
