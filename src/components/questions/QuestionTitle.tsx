883cda62b0b90431adba8c9a3daad60926bc54c8
Jaap <jaap@mrproper.dev>
2022-06-15 18:32:27 +0200

    Divider

--- src/components/questions/QuestionTitle.tsx
+++ src/components/questions/QuestionTitle.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import COLORS from '../../assets/colors';
 import FONTS from '../../assets/fonts';
+import Divider from '../generic/Divider';
 
 const QuestionTitle = (props: { text: string; accLabel?: string; fontSize?: number }) => {
   const styles = createStyleSheet(props.fontSize);
   return (
-    <Text
-      style={styles.openTextArea}
-      accessible={true}
-      accessibilityLabel={props.accLabel == undefined ? '' : props.accLabel}
-    >
-      {props.text}
-    </Text>
+    <View style={styles.container}>
+      <Text
+        style={styles.openTextArea}
+        accessible={true}
+        accessibilityLabel={props.accLabel == undefined ? '' : props.accLabel}
+      >
+        {props.text}
+      </Text>
+      <Divider width={300} height={2} margin={0} />
+    </View>
   );
 };
 
@@ -22,10 +26,12 @@ const createStyleSheet = (fontSize: number = 20) => {
       width: '100%',
       paddingTop: 0,
       paddingLeft: 5,
-      paddingBottom: 10,
       fontFamily: FONTS.semiBold,
       fontSize: fontSize,
       color: COLORS.black,
+    },
+    container: {
+      paddingBottom: 10,
     },
   });
 };
